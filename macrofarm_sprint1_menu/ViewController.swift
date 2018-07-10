04aa6e2fabdb65b0f17e9e620155348da18f2742
Kormos Adam <kormos.adam@gmail.com>
2018-07-10 14:29:35 +0200

    Initial Commit

---
+++ macrofarm_sprint1_menu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  macrofarm_sprint1_menu
+//
+//  Created by Kormos Adam on 2018. 07. 10..
+//  Copyright © 2018. Kormos Adam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
